3fda8f25b1cab9c2dcb6eeb1c0ab255d4fd43fc1
GalSanyi <galsanyi84@gmail.com>
2022-05-31 14:18:01 +0200

    form hooks

--- src/components/Form/ContactForm .js
+++ src/components/Form/ContactForm .js
@@ -1,68 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import s from './ContactForm.module.css';
 
 import shortid from 'shortid';
 
-export default class ContactForm extends Component {
-  state = { name: '', number: '' };
-  nameInputId = shortid.generate();
-  numberInputUd = shortid.generate();
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleInputChange = event => {
+  const nameInputId = shortid.generate();
+  const numberInputId = shortid.generate();
+
+  const handleInputChange = event => {
     const { name, value } = event.currentTarget;
-    this.setState({
-      [name]: value,
-    });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor={this.nameInputId}>
-            Name
-            <input
-              className={s.inputForm}
-              onChange={this.handleInputChange}
-              value={this.state.name}
-              type="text"
-              name="name"
-              id={this.nameInputId}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <label htmlFor={this.numberInputId}>
-            Number
-            <input
-              className={s.inputForm}
-              onChange={this.handleInputChange}
-              value={this.state.number}
-              type="tel"
-              name="number"
-              id={this.numberInputId}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
-          <button className={s.buttonSubmit} type="submit">
-            Add contact
-          </button>
-        </form>
-      </>
-    );
-  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={nameInputId}>
+          Name
+          <input
+            className={s.inputForm}
+            onChange={handleInputChange}
+            value={name}
+            type="text"
+            name="name"
+            id={nameInputId}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label htmlFor={numberInputId}>
+          Number
+          <input
+            className={s.inputForm}
+            onChange={handleInputChange}
+            value={number}
+            type="tel"
+            name="number"
+            id={numberInputId}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button className={s.buttonSubmit} type="submit">
+          Add contact
+        </button>
+      </form>
+    </>
+  );
 }
